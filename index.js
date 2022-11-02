3644f5ae7c2ec6036acfebe69110f268a721e69e
Chris <christopher.brighton@netnatives.co.uk>
2022-11-02 01:00:17 +0000

    main

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const NuxtLynx = require('./lib/app/NuxtLynx')
-const ModelHandler = require('./lib/handler/ModelHandler')
-const Service = require('./lib/service/Service')
+const {ModelHandler, UploadHandler} = require('./lib/handler')
+const {Service, UploadService} = require('./lib/service')
 const parse = require('./lib/utils/parse')
 const nuxtPlugins = require('./lib/plugins')
+const handyHelpers = require('handy-helpers')
 
 module.exports = {
   NuxtLynx,
   ModelHandler,
+  UploadHandler,
   Service,
+  UploadService,
   parse,
   nuxtPlugins,
+  ...handyHelpers,
 }
